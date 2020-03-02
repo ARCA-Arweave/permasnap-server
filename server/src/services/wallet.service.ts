de2de66468fabdbcd0e489cafbc346a530b2c464
benji gogan <benjamin.gogan@accenture.com>
2020-03-02 06:36:50 +0000

    cleaned up wallet.service.

--- server/src/services/wallet.service.ts
+++ server/src/services/wallet.service.ts
@@ -1,15 +1,13 @@
-import Arweave from 'arweave/node'
-import ArCrypto from 'arweave/node/lib/crypto/node-driver'
 import crypto from 'crypto'
-import PemJwk from 'pem-jwk'
 import constants from 'constants'
+import Arweave from 'arweave/node'
+import { jwk2pem } from 'pem-jwk'
+import { JWKInterface } from 'arweave/node/lib/wallet'
 
 const keyLength = 4096
 const publicExponent = 0x10001
 const hashAlgorithm = 'sha256'
 const encryptionAlgorithm = 'aes-256-cbc'
-
-const ArweaveCrypto = new ArCrypto()
 
 const instance = Arweave.init({
 	host: 'arweave.net', // Hostname or IP address for a Arweave host
@@ -22,32 +20,32 @@ const instance = Arweave.init({
 instance.wallets.generate().then(async key => {
 	const wallet_from_key = await instance.wallets.jwkToAddress(key)
 	const pub_key = key.n
-	var pem = PemJwk.jwk2pem(key)
+	var pem = jwk2pem(key)
 	const signaturePayload = 'hihi'
-	const signed_message = await sign(key, signaturePayload)
+	const signed_message = sign(key, signaturePayload)
 	console.log(signed_message)
-	verify(pub_key, signaturePayload, signed_message)
+	console.log(verify(pub_key, signaturePayload, signed_message))
 })
 
-async function sign(jwk, dataToSign) {
+function sign(jwk: JWKInterface, dataToSign: string) {
 	let rawSignature = crypto
 		.createSign(hashAlgorithm)
 		.update(dataToSign)
 		.sign({
-			key: PemJwk.jwk2pem(jwk),
+			key: jwk2pem(jwk),
 			padding: constants.RSA_PKCS1_PSS_PADDING,
 			saltLength: 0
 		})
 	return rawSignature
 }
 
-async function verify(publicModulus, data, signature) {
+function verify(publicModulus: string, data: string, signature: Buffer): boolean {
 	const publicKey = {
 		kty: 'RSA',
 		e: 'AQAB',
 		n: publicModulus
 	}
-	const pem = PemJwk.jwk2pem(publicKey)
+	const pem = jwk2pem(publicKey)
 	return crypto
 		.createVerify(hashAlgorithm)
 		.update(data)
