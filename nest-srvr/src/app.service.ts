33bd6acd90a70779ea728e5275d31b4c6af7c346
benji gogan <benjamin.gogan@accenture.com>
2020-03-11 23:36:27 +0000

    .

--- nest-srvr/src/app.service.ts
+++ nest-srvr/src/app.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { ClientDelegatedTxnDto } from './types/types'
 import log from './utils/logger'
+import { WalletProvider } from './wallet.provider'
 
 @Injectable()
 export class AppService {
+	constructor(private readonly walletProvider: WalletProvider) {}
 	async delegatedPost(delegated_post_data: ClientDelegatedTxnDto): Promise<string> {
 		log.log(delegated_post_data)
 		return 'Hello World!'
