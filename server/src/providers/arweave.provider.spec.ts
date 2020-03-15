fd2f87c542fda301f880d9f7a2fd894081ef0a8c
benji gogan <benjamin.gogan@accenture.com>
2020-03-15 12:46:01 +0000

    updated tests

--- server/src/providers/arweave.provider.spec.ts
+++ server/src/providers/arweave.provider.spec.ts
@@ -1,18 +1,42 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ArweaveProvider } from './arweave.provider';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ArweaveProvider } from './arweave.provider'
 
-describe('Wallet', () => {
-  let provider: ArweaveProvider;
+describe('Arweave Provider', () => {
+	let provider: ArweaveProvider
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ArweaveProvider],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [ArweaveProvider]
+		}).compile()
 
-    provider = module.get<ArweaveProvider>(ArweaveProvider);
-  });
+		provider = module.get<ArweaveProvider>(ArweaveProvider)
+	})
 
-  it('should be defined', () => {
-    expect(provider).toBeDefined();
-  });
-});
+	/** verifyHash */
+
+	it('verifyHash - verification should pass', () => {
+		// expect(provider).toBeDefined()
+	})
+
+	it('verifyHash - verification should fail', () => {
+		expect(provider).toBeDefined()
+	})
+
+	/** checkPostExists */
+
+	it('checkPostExists - should find existing post', () => {})
+
+	it('checkPostExists - should fail to find existing post', () => {})
+
+	/** verifyOwnership */
+
+	it('verifyOwnership - verification should pass', () => {})
+
+	it('verifyOwnership - verification should fail', () => {})
+})
+
+// describe('root', () => {
+// 	it('should return "Hello World!"', async () => {
+// 		expect(await appController.delegatedPost()).toBe('Hello World!')
+// 	})
+// })
