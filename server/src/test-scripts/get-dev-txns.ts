575c8735fd2d4afcaa762a49bb8743bee26515aa
benji gogan <benjamin.gogan@accenture.com>
2020-03-16 22:59:15 +0000

    verify ownership works

--- server/src/test-scripts/get-dev-txns.ts
+++ server/src/test-scripts/get-dev-txns.ts
@@ -7,21 +7,18 @@ const arweaveProvider = new ArweaveProvider(new WalletProvider())
 
 const run = async () => {
 	const d_posts = await arweaveProvider.getAllDelegatedPosts({ psnap_app_version: '0.1.3', psnap_context: 'development' })
-	// log.log(d_posts)
+	log.log(d_posts)
 	if (!d_posts.length) return
 	const txns = await arweaveProvider.getPostData(d_posts)
 
 	const results: boolean[] = []
-	// txns.forEach((txn: ClientDelegatedTxnDto) => {
-	// 	results.push(arweaveProvider.verifyHash(txn))
-	// })
+
 	const delegated_post_data = txns[0]
-	// console.log(delegated_post_data)
 
 	const { dpost_hash, dpost_owner, dpost_signature } = delegated_post_data
 	const verifyHash = arweaveProvider.verifyHash(delegated_post_data)
 	const verifyOwnership = arweaveProvider.verifyOwnership(dpost_owner, dpost_hash, new Buffer(dpost_signature, 'base64'))
-
+	// log.log(delegated_post_data)
 	log.log(`verifyHash: ${verifyHash}`)
 	log.log(`verifyOwnership: ${verifyOwnership}`)
 }
