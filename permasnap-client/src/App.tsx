5280ec9f855cfb6ba382babd46bd8c9e712f7ff1
Ros McMahon <rosmcmahon@gmail.com>
2020-03-31 18:03:59 +0100

    wallet <-> redux

--- permasnap-client/src/App.tsx
+++ permasnap-client/src/App.tsx
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonRouterOutlet,
-  IonFab,
-  IonFabButton,
-  isPlatform
-} from '@ionic/react';
+import { IonApp, IonRouterOutlet, IonFab, IonFabButton, isPlatform } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Plugins } from "@capacitor/core";
 import Tab1 from './pages/Tab1';
@@ -15,12 +9,10 @@ import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
 /* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
 /* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
@@ -28,7 +20,6 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
 /* Theme variables */
 import './theme/variables.css';
 
@@ -40,9 +31,10 @@ if(isPlatform('android')){
 }
 
 const App: React.FC = () => {
-  useEffect(()=>{
+  useEffect(() => {
     Plugins.SplashScreen.hide()
-  },[])//like c'tor
+    console.log('THIS TEXT SHOULD ONLY PRINT ONCE')
+  },[]) //like c'tor
   return (
     <IonApp>
       <IonReactRouter>
