db7ae5246f926e036e544ad82e20c228f51c073e
benji gogan <benjamin.gogan@accenture.com>
2020-04-21 00:10:11 +0100

    cors

--- server/src/providers/wallet.provider.ts
+++ server/src/providers/wallet.provider.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import wallet from '../secrets/arweave-keyfile.json'
+import wallet from 'src/secrets/arweave-keyfile.json'
 import log from '../utils/logger'
 
 @Injectable()
