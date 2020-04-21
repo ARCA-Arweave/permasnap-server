4be59ba80a161cdd26c71528aebbee323719b1b0
Ros McMahon <rosmcmahon@gmail.com>
2020-04-21 10:00:33 +0100

    removed all client code

--- permasnap-client/src/hooks/useServer.test.ts
+++
@@ -1,3 +0,0 @@
-import { ClientDelegatedTxnDto } from "../../../server/src/types/dto"; 
-import testImage from "../assets/img/image.test.png";
-
--- permasnap-client/src/redux/actions/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './actionTypes';
-export * from './todos';
-export * from './wallet';
