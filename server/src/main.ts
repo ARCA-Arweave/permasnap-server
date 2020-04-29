a8b6a73e67c6c32be1b077874e3e860586c7e0c3
Ros McMahon <rosmcmahon@gmail.com>
2020-04-29 19:39:18 +0100

    fix [ExceptionsHandler] request entity too large

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { ValidationPipe } from '@nestjs/common'
+import * as bodyParser from 'body-parser'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
@@ -14,9 +15,9 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, options)
 	SwaggerModule.setup('api', app, document)
 	app.useGlobalPipes(new ValidationPipe())
-	
+	app.use(bodyParser.json({limit: '7mb'}));
+  app.use(bodyParser.urlencoded({limit: '7mb', extended: true}));
 	app.enableCors();
-
 	await app.listen(3000)
 }
 bootstrap()
